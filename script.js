76f61bc27b0cbdbb44d2ff2adb2f8afab28e18d6
analebyodkina <dp030992cad@gmail.com>
2023-02-26 15:55:41 +0200

    added animations, fixed bugs, finished adaptivity

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ function navToggle() {
     var headerUl = document.getElementById("headerUl");
     headerUl.classList.toggle("active");
   }
+  function playToggle() {
+    var headerUl = document.getElementById("playButton");
+    headerUl.classList.toggle("active");
+  }
